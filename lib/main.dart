d92446bea0684252f382e431857dccd9017ecb85
divaariani <divaariani@apps.ipb.ac.id>
2025-06-07 12:49:21 +0700

    implement firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recipes/theme/app_colors.dart';
 import 'package:recipes/utils/app_router.dart';
+import 'package:recipes/utils/app_firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const MyApp());
 }
 
